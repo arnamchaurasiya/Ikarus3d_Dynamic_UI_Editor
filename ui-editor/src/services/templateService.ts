d2887af38f9da20e98e3607ac5cb5df40cd120cb
arnamchaurasiya <115061541+arnamchaurasiya@users.noreply.github.com>
2025-10-19 09:13:37 +0530

    Update TEMPLATES_API_URL to new GitHub source

--- ui-editor/src/services/templateService.ts
+++ ui-editor/src/services/templateService.ts
@@ -13,7 +13,7 @@ export interface TemplateContent {
   css: string;
 }
 
-const TEMPLATES_API_URL = 'https://raw.githubusercontent.com/lazerpay/webbuilder-templates/main/index.json';
+const TEMPLATES_API_URL = 'https://arnamchaurasiya.github.io/Ikarus3d_Dynamic_UI_Editor/templates.json';
 
 export const templateService = {
   async fetchTemplates(): Promise<Template[]> {
